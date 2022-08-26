72f5e54e689c5ea1555f6e23f873d164cba880dd
pepriandika <pepriandika@gmail.com>
2022-08-26 18:56:50 +0700

    increasment-decreasement App

--- app/src/main/java/com/programersma/projectbaru/MainActivity.kt
+++ app/src/main/java/com/programersma/projectbaru/MainActivity.kt
@@ -2,12 +2,30 @@ package com.programersma.projectbaru
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.programersma.projectbaru.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var angkaBerubah = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button3.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Hai, Aku Pepri Andika",Toast.LENGTH_LONG).show()
+        }
+        binding.tombol2.setOnClickListener {
+            angkaBerubah++
+            binding.bebas.text = angkaBerubah.toString()
+        }
+        binding.tombol1.setOnClickListener {
+            angkaBerubah--
+            binding.bebas.text = angkaBerubah.toString()
+        }
     }
-
-
-}
+//
+    }
